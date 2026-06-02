c1b6024920878dce72a425022c945b23137e1e8c
shahzaib57s-test <azharsaleem007@gmail.com>
2026-06-02 16:17:27 +0200

    v1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("keydown", (event) => {
+    console.log(event.key);
+
+    if(event.key == "key"){
+        //document.getElementById("").style = "color:blue";
+    }
+    else{
+     //document.body.style = "background-color:red";
+    }
+});
