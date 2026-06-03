fda4f5743136ba25f6a674208b4bc3dc7c85f062
shahzaib57s-test <azharsaleem007@gmail.com>
2026-06-03 14:01:44 +0200

    V5
    JS

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 document.addEventListener("keydown", (event) => {
     console.log(event);
+    document.getElementById(event.key).style.backgroundColor = "yellow";
+})
+
+
+    document.addEventListener("keyup", (event) => {
+        console.log(event);
+        document.getElementById(event.key).style.backgroundColor = "white"
+})
