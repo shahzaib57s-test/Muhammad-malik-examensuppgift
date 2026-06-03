bdb705293910e0ad6549c2616f41e5df353da177
shahzaib57s-test <azharsaleem007@gmail.com>
2026-06-03 16:28:40 +0200

    V8
    a

--- js/script.js
+++ js/script.js
@@ -6,6 +6,19 @@ document.addEventListener("keydown", (event) => {
     audio.src = "filer/freesound_community-b6-82017.mp3"
     document.body.appendChild(audio)
     audio.play()
+
+    
+    let audio1 = document.createElement("audio")
+    audio.src = "filer/[Fang Yuan]“Thes......t for (1).mp3"
+    document.body.appendChild(audio)
+    audio.play()
+
+    document.addEventListener("keydown", (event) => {
+        console.log(event)
+        document.getElementById("KeyS").style.backgroundColor = "var(--color)"
+
+      
+    })
 })
 
 
