44ed08eb3087302048ce1e1c4e8a5e926ec9c310
shahzaib57s-test <azharsaleem007@gmail.com>
2026-06-03 16:08:25 +0200

    v7
    klart med det som var obligatorisk, la till audio

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 document.addEventListener("keydown", (event) => {
-    console.log(event);
-    document.getElementById(event.code).style.backgroundColor = "gold";
+    console.log(event)
+    document.getElementById(event.code).style.backgroundColor = "var(--color)"
+
+    let audio = document.createElement("audio")
+    audio.src = "filer/freesound_community-b6-82017.mp3"
+    document.body.appendChild(audio)
+    audio.play()
 })
 
 
-    document.addEventListener("keyup", (event) => {
-        console.log(event);
-        document.getElementById(event.code).style.backgroundColor = "white"
+document.addEventListener("keyup", (event) => {
+    console.log(event);
+    document.getElementById(event.code).style.backgroundColor = "white"
 })
